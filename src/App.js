f888e521539f48ba3111c69a0e87381d8d2d8505
MarcHofstetter <marc.hofstetter@lernende.bbw.ch>
2023-06-27 16:55:31 +0200

    :))))))))))((

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from 'react-bootstrap/Form';
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(){
+
+    const [selectedAnswer, setSelectedAnswer] = useState('');
+
+    const handleAnswerChange = (event) => {
+        setSelectedAnswer(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+    return (
+        <div>
+            <h3>Ein Tennisschläger und ein Ball kosten zusammen 110 CHF. Der Schläger ist genau 100 CHF teurer als der Ball. Wie teuer ist der Schläger?</h3>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <input
+                        type="radio"
+                        value="10"
+                        checked={selectedAnswer === '10'}
+                        onChange={handleAnswerChange}
+                    />
+                    10 CHF
+                </label>
+                <br />
+                <label>
+                    <input
+                        type="radio"
+                        value="50"
+                        checked={selectedAnswer === '50'}
+                        onChange={handleAnswerChange}
+                    />
+                    50 CHF
+                </label>
+                <br />
+                <label>
+                    <input
+                        type="radio"
+                        value="105"
+                        checked={selectedAnswer === '105'}
+                        onChange={handleAnswerChange}
+                    />
+                    105 CHF
+                </label>
+                <br />
+                <label>
+                    <input
+                        type="radio"
+                        value="110"
+                        checked={selectedAnswer === '110'}
+                        onChange={handleAnswerChange}
+                    />
+                    110 CHF
+                </label>
+                <br />
+                <button type="submit">Antwort überprüfen</button>
+            </form>
+        </div>
+
+    );
+};
 
 export default App;
